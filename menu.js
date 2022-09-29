ff8edc83f2982f113776b473352af6c8d98fac9a
Mathieu LGS <mathieu.langlois@voxm.live>
2022-09-29 17:17:55 +0200

    Working on index style and img

---
+++ menu.js
@@ -0,0 +1,5 @@
+const entreesMenu = [ "Salade de tomates","Mozarella - Roquette","Chèvres chauds","Antipasta italiens"]
+const platsMenu = [ "Pizza Buffala","Pâtes aux légumes du soleil","Escalope Milanaise","Canard à l'italienne"]
+const dessertsMenu = [ "Tiramisu","Glace à la vanille","Glace au chocolat","Glace au café"]
+const boissonsMenu = [ "Coca","Fanta","Sprite","Eau"]
+
